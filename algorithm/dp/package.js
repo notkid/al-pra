739477f2ae0e0fdffd087798d1e74041f7918128
notkid <liuhaiyang1992@hotmail.com>
2021-03-23 17:39:54 +0800

    a

--- algorithm/dp/package.js
+++ algorithm/dp/package.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-03-22 19:44:35
- * @LastEditTime: 2021-03-22 19:59:35
+ * @LastEditTime: 2021-03-23 17:39:50
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \leetcode\algorithm\dp\package.js
@@ -25,13 +25,65 @@ var canPartition = function (nums) {
     if (sum % 2 !== 0) return false
     sum = sum / 2
 
+    const dp = new Array(sum + 1).fill(false)
     dp[0] = true
-    for (let i = 0; i < n; i++) {
+    for (const num of nums) {
         for (let j = sum; j >= 0; j--) {
-             if (j - nums[i] >= 0) {
-                 dp[j] = dp[j] || dp[j -nums[i]]
-             }
+            if (j - num >= 0) {
+                dp[j] = dp[j] || dp[j - num]
+            }
         }
     }
     return dp[sum]
 };
+
+/**
+ * 
+ * 494. 目标和
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} S
+ * @return {number}
+ */
+var findTargetSumWays = function (nums, S) {
+    let count = 0
+    function calculate(nums, i, sum, S) {
+        if (i === nums.length) {
+            if (sum === S) {
+                count++
+            }
+            return
+        } else {
+            calculate(nums, i + 1, sum + nums[i], S)
+            calculate(nums, i - 1, sum - nums[i], S)
+        }
+    }
+    calculate(nums, 0, 0, S)
+    return count
+};
+
+// console.log(findTargetSumWays([1, 1, 1, 1, 1], 3))
+
+function findTargetSumWays2(nums, S) {
+
+}
+
+// 
+let arr = []
+function dp(level, parentChoice) {
+    if (level === 0) {
+        arr.push(parentChoice)
+        return
+    }
+    ['A', 'B', '', 'D'].forEach(a => {
+        dp(level - 1, parentChoice + a)
+    })
+}
+// dp(6, '')
+// let a = arr
+//     .filter(a => !a.includes('D'))
+//     .map(a => a.replace('D', ''))
+//     .filter(a => a.length >= 3)
+// console.log(a.length / arr.length)
