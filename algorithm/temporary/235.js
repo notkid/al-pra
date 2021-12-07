b5ce2a42d59a89f65ed806255b13885e396e08a2
hyLiu <liuhaiyang1992@hotmail.com>
2021-12-07 23:06:57 +0800

    five

---
+++ algorithm/temporary/235.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * 235. 二叉搜索树的最近公共祖先
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+ var lowestCommonAncestor = function(root, p, q) {
+    while (root) {
+        if (root.val > p.val && root.val > q.val) {
+            root = root.left
+        } else if (root.val < p.val && root.val < q.val) {
+            root = root.right
+        } else {
+            break
+        }
+    }
+    return root 
+};
