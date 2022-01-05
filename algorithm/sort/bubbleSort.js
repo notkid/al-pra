acee31c273e3a6711f61b0c1bab1ea960d72565d
notkid <liuhaiyang1992@hotmail.com>
2022-01-05 19:25:05 +0800

    4

--- algorithm/sort/bubbleSort.js
+++ algorithm/sort/bubbleSort.js
@@ -7,9 +7,7 @@ function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        let tmp = arr[j + 1];
-        arr[j + 1] = arr[j];
-        arr[j] = tmp;
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
