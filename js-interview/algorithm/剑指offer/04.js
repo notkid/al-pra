74049fd07f6e3fb0bb37a3e4eb3ee2f87b163a30
haiyangliu <liuhaiyang1992@hotmail.com>
2022-12-05 17:36:41 +0800

    a

---
+++ js-interview/algorithm/剑指offer/04.js
@@ -0,0 +1,17 @@
+/**
+ * 剑指 Offer 04. 二维数组中的查找
+ */
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+ var findNumberIn2DArray = function(matrix, target) {
+    let iLeft = 0,jLeft = 0,iRight = matrix.length -1,jRight = matrix[0].length -1
+    while() {
+        let midLeft = Math.floor((iLeft+iRight) /2)
+        let midRight = Math.floor((jLeft+jRight) /2)
+        
+    }
+};
